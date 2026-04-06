f61c94bf5abf957d781adcbb3c53922665864d97
cjabendan <cjamesabendan@gmail.com>
2026-04-06 17:35:14 +0800

    feat(context): implement global theme conteaxt for theme-toggle

--- web/src/components/theme-toggle.tsx
+++ web/src/components/theme-toggle.tsx
@@ -1,34 +1,22 @@
 import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
-
-
-// To do: Transform this into a Global Context
+import { useTheme } from "@/context/ThemeContext" 
 
 export function ThemeToggle() {
-  
-  const [isDark, setIsDark] = useState(true)
-
-  useEffect(() => {
-    const root = window.document.documentElement
-    if (isDark) {
-      root.classList.add("dark")
-    } else {
-      root.classList.remove("dark")
-    }
-  }, [isDark])
+  const { theme, toggleTheme } = useTheme(); 
+  const isDark = theme === "dark";
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setIsDark(!isDark)}
+      onClick={toggleTheme}
       className="rounded-xl w-10 h-10 border border-lines text-description hover:bg-tint/5 hover:text-foreground transition-all active:scale-95"
     >
       {isDark ? (
-        <Moon className="h-5 w-5 transition-transform rotate-0 scale-100" />
+        <Moon className="h-5 w-5 transition-transform" />
       ) : (
-        <Sun className="h-5 w-5 transition-transform rotate-0 scale-100" />
+        <Sun className="h-5 w-5 transition-transform" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
